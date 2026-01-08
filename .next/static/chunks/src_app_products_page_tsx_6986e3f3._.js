5dd1883f2c519b3c01d2efe6138e7c112ad3d034
Shukladev7 <exit08006@gmail.com>
2026-01-08 11:47:31 +0530

    first commit

--- .next/static/chunks/src_app_products_page_tsx_6986e3f3._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_cce69015._.js",
-  "static/chunks/node_modules_3de07684._.js"
-],
-    source: "dynamic"
-});
